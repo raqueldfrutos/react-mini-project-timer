e12818c24f23d4e7ab043e475dc22b80acfd20fe
raqueldfrutos <raqueldfrutos@gmail.com>
2026-01-12 15:50:09 +0100

    setInterval

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,30 @@ import { useState, useRef } from "react";
 
 const App = () => {
   const timerRef = useRef(null);
-  const [time, setTime] = useState(0); //time is the variables that saves the display value
+  const [time, setTime] = useState(0); //time is the variable that saves the display value
   const [isRunning, setIsRunning] = useState(false);
+
+  const toggleTimer = () => {
+    if (isRunning) {
+      //  Clear interval to stop the timer
+      clearInterval(timerRef.current);
+      timerRef.current = null;
+    } else {
+      // Star timer
+      timerRef.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000); // we are incresing 1 every 1 milisecond = 1 second. setInterval returns an Id which we are puttin to the ref
+    }
+    setIsRunning(!isRunning);
+  };
+
   return (
     <div className="max-w-md mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg text-center">
       <h2 className="text-4xl font-semibold mt-4">⏳Timer {time}</h2>
-      <button className="mt-3 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
+      <button
+        onClick={toggleTimer}
+        className="mt-3 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+      >
         {isRunning ? "Pause" : "Start"}
       </button>
     </div>
@@ -15,3 +33,5 @@ const App = () => {
 };
 
 export default App;
+
+// Notes: if we dont use ref here, the button will start the timer but wont pause it, because the value wont persist across the state
