e424779d84da297643f3e9515c14aa9827ecdbe3
raqueldfrutos <raqueldfrutos@gmail.com>
2026-01-13 16:55:55 +0100

    Add focus() to Start button

--- src/components/TimerControls.jsx
+++ src/components/TimerControls.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useRef } from "react";
+
 const TimerControls = ({ toggleTimer, isRunning, resetTimer }) => {
+  const startButtonRef = useRef(null);
+
+  useEffect(() => {
+    if (startButtonRef.current) {
+      startButtonRef.current.focus();
+    }
+  }, []);
   return (
     <>
       <button
+        ref={startButtonRef}
         onClick={toggleTimer}
         className="mt-3 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 mr-3"
       >
