547b15d354b79964dd1d57f6052766bf2889d63c
raqueldfrutos <raqueldfrutos@gmail.com>
2026-01-13 16:57:24 +0100

    Focus()

--- src/components/TimerControls.jsx
+++ src/components/TimerControls.jsx
@@ -7,11 +7,11 @@ const TimerControls = ({ toggleTimer, isRunning, resetTimer }) => {
     if (startButtonRef.current) {
       startButtonRef.current.focus();
     }
-  }, []);
+  }, []); // when the component mounts, the button element is selected by the useRef and we had focus() to it.
   return (
     <>
       <button
-        ref={startButtonRef}
+        ref={startButtonRef} // selecting the DOM element of the button
         onClick={toggleTimer}
         className="mt-3 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 mr-3"
       >
